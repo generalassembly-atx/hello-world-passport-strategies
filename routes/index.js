8ad79ced69b133776c52c11b5f73a1e8bee29a73
Christian Mansour <c.mansour@miami.edu>
2016-07-13 18:41:57 -0500

    oauth completed

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,27 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: req.user });
+  res.render('index', { title: 'Christian\'s Oauth', user: req.user });
 });
 
 router.post('/login',
   passport.authenticate('local', { successRedirect: '/',
                                    failureRedirect: '/' })
 );
+
+router.get('/auth/github',
+  passport.authenticate('github')
+);
+
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+   function(req, res) {
+
+    res.redirect('/');
+  });
+
+
+
+
 
 module.exports = router;
