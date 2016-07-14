12a1d5fbf276e3c0853047b5bc1ddc30b8a12b4f
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-07-13 21:11:09 -0500

    finish homework

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,15 @@ router.post('/login',
                                    failureRedirect: '/' })
 );
 
+router.get('/auth/github',
+  passport.authenticate('github'));
+
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+
 module.exports = router;
